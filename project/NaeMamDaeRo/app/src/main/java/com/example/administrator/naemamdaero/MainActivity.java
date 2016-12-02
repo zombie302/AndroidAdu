cb03c281fd6d2276c5b1941e430aea414108a68b
androideduTester <kyunghyun.ko@m3solutions.co.kr>
2016-12-03 01:40:22 +0900

    [강사]초기 코드 작성
    1. testRoot 삭제후 MainActivity로 코드 이동
    2. 각 버튼에 연결할 Activity 의 담당자 이름 명시함.
    3. Dialog 예제 코드 추가

--- project/NaeMamDaeRo/app/src/main/java/com/example/administrator/naemamdaero/MainActivity.java
+++ project/NaeMamDaeRo/app/src/main/java/com/example/administrator/naemamdaero/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.naemamdaero;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startActivity(Class cls)
+    {
+        Intent i = new Intent(this, cls);
+        this.startActivity(i);
+    }
+
+    public void onIntro(View v)
+    {
+    }
+
+    public void onDiaryList(View v)
+    {
+    }
+
+    public void onDiaryView(View v)
+    {
+    }
+
+    public void onDiaryEdit(View v)
+    {
+    }
+
+    public void onDiaryInsert(View v)
+    {
+    }
+
+    public void onCategoryList(View v)
+    {
+    }
+
+    public void onCategoryDiaryList(View v)
+    {
+    }
+
+    public void onCategoryAddDiaryList(View v)
+    {
+    }
+
+    public void onCategoryInsertDlg(View v)
+    {
+    }
+
+    public void onCategoryListDlg(View v)
+    {
+        MyDialog dialog = new MyDialog(this);
+        dialog.show();
+    }
+
+
 }
